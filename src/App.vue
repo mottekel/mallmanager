ccefa458e5d8c7f4d34f1a720ebad7cbab730f7b
mottekel <2792936441@qq.com>
2022-03-22 09:21:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <el-button type="success">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
